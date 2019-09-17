62aa70f3bf2223349e16c255a11fd18a9dfde5fa
FFTCMJBJ054 <jaybeejay49@gmail.com>
2019-09-17 14:43:52 +0100

    new adjustments

--- team-dauntless/login.php
+++ team-dauntless/login.php
@@ -20,7 +20,7 @@
 			$msg="<span style='color:red'>Invalid Login Details</span>";
 		}
 	}
-?> 
+?>
 <!DOCTYPE html>
 
 <head>
@@ -34,63 +34,64 @@
 </head>
 
 <body>
-    <section id="image-banner">
-        <div id="logo-header">
-            <img src="./assets/img/logo.PNG" width="30" alt="logo">
-        <h1>DAUNTFAST</h1>
-        </div>
-        <em>people who love food are usually good people...</em>
-    </section>
+    
+        <section id="image-banner">
+            <div id="logo-header">
+                <img src="./assets/img/logo.PNG" width="30" alt="logo">
+                <h1>DAUNTFAST</h1>
+            </div>
+            <em>people who love food are usually good people...</em>
+        </section>
 
-    <section id="wrapper">
-        <div class="form-cards">
-            <section id="back">
-                <div class="sign-up-container">
-                    <form action="create.php">
-                        <h1>Create Account</h1>
-                        <div class="social-container">
-                            <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your email for registration</span>
-                        <input type="text" placeholder="Name" name="name" required />
-                        <input type="email" placeholder="Email" name="email" required/>
-                        <input type="password" placeholder="Password" name="password" required/>
-                        <button>Sign Up</button>
+        <section id="wrapper">
+            <div class="form-cards">
+                <section id="back">
+                    <div class="sign-up-container">
+                        <form action="create.php">
+                            <h1>Create Account</h1>
+                            <div class="social-container">
+                                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                            <span>or use your email for registration</span>
+                            <input type="text" placeholder="Name" name="name" required />
+                            <input type="email" placeholder="Email" name="email" required />
+                            <input type="password" placeholder="Password" name="password" required />
+                            <button id = "sign-up-btn">Sign Up</button>
 
-                        <h5 class="signing">Want to <span class="sign-in">Sign in</span> instead?</h5>
-                    </form>
-                   
-                </div>
-            </section>
-            <section id="front">
-                <div class=" sign-in-container">
-                    <form action="" method="post" name="Login_Form">
-                        <h1>Sign in</h1>
-                        <div class="social-container">
-                            <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-                            <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
-                        </div>
-                        <span>or use your account</span>
-                        <?php if(isset($msg)){?>
+                            <h5 class="signing">Want to <span class="sign-in">Sign in</span> instead?</h5>
+                        </form>
+
+                    </div>
+                </section>
+                <section id="front">
+                    <div class=" sign-in-container">
+                        <form action="" method="post" name="Login_Form">
+                            <h1>Sign in</h1>
+                            <div class="social-container">
+                                <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+                                <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                            <span>or use your account</span>
+                            <?php if(isset($msg)){?>
                             <span style="color:red;"><?php echo $msg;?></span>
                             <?php } ?>
-                        <input type="username" placeholder="Username" name="Username" required/>
-                        <input type="password" placeholder="Password" name="Password" required/>
-                        <a href="#">Forgot your password?</a>
-                        <button type="submit" name="Submit" value="Login">Sign In</button>
-                        <h5 class="signing">Want to <span class="sign-up">Sign up</span> instead?</h5>
-                    </form>
-                    
-                </div>
-            </section>
-        </div>
-    </section>
-    <footer><h2>Made By Team Dauntless</h2></footer>
+                            <input type="username" placeholder="Username" name="Username" required />
+                            <input type="password" placeholder="Password" name="Password" required />
+                            <a href="#">Forgot your password?</a>
+                            <button type="submit" name="Submit" value="Login">Sign In</button>
+                            <h5 class="signing">Want to <span class="sign-up">Sign up</span> instead?</h5>
+                        </form>
+
+                    </div>
+                </section>
+            </div>
+        </section>
+    
+    <footer>Made By Team Dauntless</footer>
     <script src="./assets/js/form.js"></script>
 </body>
 
 </html>
-
